ebba93d10efd566284bba7db16b69ac100d56798
Victor Peralta <victor.peralta.gomez@gmail.com>
2021-11-16 12:47:20 -0600

    Shuffle working

--- scripts.js
+++ scripts.js
@@ -1,20 +1,38 @@
 
 let toDoItems = ['Go to Laundry', 'Go to Supermarket',
-    'Go to Doctor', 'Wash dishes', 'Debug code','Make inventory'];
-loadItems();
-    let btnShuffle = document.getElementById('shuffle-button');
+    'Go to Doctor', 'Wash dishes', 'Debug code', 'Make inventory'];
+loadItems(toDoItems);
+let btnShuffle = document.getElementById('shuffle-button');
 btnShuffle.addEventListener('click', shuffleClicked);
 
 function shuffleClicked() {
-
+    console.log('clicked');
+    loadItems(shuffle(toDoItems));
 }
 
-function loadItems(){
-    let listItems=document.getElementById('listItems');
-    listItems.innerHTML='';
-    for(let i=0;i<toDoItems.length;i++){
-        let li=document.createElement('li');
-        li.innerText=toDoItems[i];
+function shuffle(items) {
+    let newArray = [];
+    let indexUsed = [];
+    console.log('shuffling');
+    let size = items.length;
+    for (i = 0; i < items.length; i++) {
+        let t = 0;
+        do {
+            t = Math.floor(Math.random() * size);
+        }
+        while (indexUsed.includes(t));
+        indexUsed.push(t);
+        newArray.push(items[t]);
+    }
+    return newArray;
+}
+
+function loadItems(items) {
+    let listItems = document.getElementById('listItems');
+    listItems.innerHTML = '';
+    for (let i = 0; i < items.length; i++) {
+        let li = document.createElement('li');
+        li.innerText = items[i];
         listItems.appendChild(li);
     }
 }
